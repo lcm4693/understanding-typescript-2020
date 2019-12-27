adf1fc3f15dc260f37e2dd9d5520ac302e579f85
Diego Serpa <diego.neves@serpro.gov.br>
2019-12-27 15:10:32 -0300

    Literal types - Aula 22

--- core-types-code/app.ts
+++ core-types-code/app.ts
@@ -1,18 +1,33 @@
 // My function accepts two arguments types (number or string)
 // It's called union type.
-function combine(input1: number | string, input2: number | string){
-    let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
-    }else{
-        result = input1.toString() + input2.toString();
-    }
+function combine(
+  input1: number | string,
+  input2: number | string,
 
-    return result;
+  //I'm using union type combined with literal type.
+  //Here, this variable accepts only these two values.
+  resultConversion: 'as-number' | 'as-text'
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+//   if(resultConversion === 'as-number'){
+//       //Converts a string to a number
+//     return +result;
+//   }else{
+      return result.toString();
+//   }
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineStrings = combine('Diego', 'Serpa');
+const combineStringAges = combine("30", "26", "as-number");
+console.log(combineStringAges);
+
+const combineStrings = combine("Diego", "Serpa", "as-text");
 console.log(combineStrings);
