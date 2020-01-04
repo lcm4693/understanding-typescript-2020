4161841977981a3f853328dc15842f592c66c1b7
Diego Serpa <diegos.java@gmail.com>
2020-01-03 23:58:58 -0300

    Use tsconfig

--- compiler/app.ts
+++ compiler/app.ts
@@ -1,5 +1,7 @@
 // If I compile my application using -w I'll able to recompile automatically my code.
 // Ex: tsc app.ts -w
+// But I can create the tsconfig.json with tsc --init,
+// after that all files will be compiled using the command 'tsc'.
 
 const userName = 'Diego Serpa';
 
