525afeb50780ba3c98790258c3a975a3e72f7904
Diego Serpa <diego.neves@serpro.gov.br>
2020-01-06 20:17:51 -0300

    Created a project for section 4

---
+++ section-4/src/app.ts
@@ -0,0 +1,20 @@
+// If I compile my application using -w I'll able to recompile automatically my code.
+// Ex: tsc app.ts -w
+// But I can create the tsconfig.json with tsc --init,
+// after that all files will be compiled using the command 'tsc'.
+
+const userName = 'Diego Serpa';
+
+console.log(userName);
+
+// The code below will fail, because there is a validation 'strictNullChecks',
+// but we can use the ! signal for tell to typescript that we are sure that the value won't be null.
+const button = document.querySelector('button')!;
+
+function clickHandler(message: string){
+    // let nome = 'Diego';
+    console.log('Clicked! ' + message);
+}
+
+button.addEventListener('click', clickHandler.bind(null, 'Teste'));
+
