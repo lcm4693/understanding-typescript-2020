69781684110d74f4ff63e7d2ee9208540e88a03f
Diego Serpa <diegos.java@gmail.com>
2020-01-10 15:52:01 -0300

    private and public modifiers

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -1,6 +1,7 @@
 class Department {
-  name: string;
-  size: number;
+  private name: string;
+  private size: number;
+  private employees: string[] = []
 
   constructor(name: string, size: number) {
     this.name = name;
@@ -12,14 +13,30 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name + ". Size: " + this.size);
   }
+
+  addEmployee(employee: string){
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation(){
+      console.log(this.employees.length, this.employees);
+  }
+
 }
 
 const departamento = new Department("Marketing", 15);
-
 departamento.describe();
 
 // Aqui eu criei um objeto anônimo com o atributo name e o método describe
 // O parâmetro describe exige que a instância seja uma instância compatível com Department.
 // por isso o atributo name é obrigatório, afinal o construtor exige. (o size também)
-const departamentoCopia = { name: "Sales", size: 10, describe: departamento.describe };
-departamentoCopia.describe();
+
+// const departamentoCopia = { name: "Sales", size: 10, describe: departamento.describe };
+// departamentoCopia.describe();
+
+// A propriedade name está marcada como 'private', logo não é possível acessá-la.
+// departamento.name = 'Contability';
+
+departamento.addEmployee('Diego');
+departamento.addEmployee('Livia');
+departamento.printEmployeeInformation();
