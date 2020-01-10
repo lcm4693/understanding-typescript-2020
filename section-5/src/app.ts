ecbf93b44436bef13f8bc7975556022e6acaaccd
Diego Serpa <diegos.java@gmail.com>
2020-01-10 15:15:40 -0300

    Section 5 init

---
+++ section-5/src/app.ts
@@ -0,0 +1,11 @@
+class Department {
+    name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+}
+
+const departamento = new Department('Marketing');
+
+console.log(departamento);
