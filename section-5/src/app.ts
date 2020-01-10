5d38a75655f86806ce3f3137f7b8144a8de20c2d
Diego Serpa <diegos.java@gmail.com>
2020-01-10 15:39:53 -0300

    Cosntructor and this keyword - Aula 60

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -1,11 +1,25 @@
 class Department {
-    name: string;
+  name: string;
+  size: number;
 
-    constructor(name: string){
-        this.name = name;
-    }
+  constructor(name: string, size: number) {
+    this.name = name;
+    this.size = size;
+  }
+
+  // Esse é um parâmetro burro só pra impedir que outras cópias tentem executar esse método,
+  //sem ter uma instância em estado válido
+  describe(this: Department) {
+    console.log("Department: " + this.name + ". Size: " + this.size);
+  }
 }
 
-const departamento = new Department('Marketing');
+const departamento = new Department("Marketing", 15);
 
-console.log(departamento);
+departamento.describe();
+
+// Aqui eu criei um objeto anônimo com o atributo name e o método describe
+// O parâmetro describe exige que a instância seja uma instância compatível com Department.
+// por isso o atributo name é obrigatório, afinal o construtor exige. (o size também)
+const departamentoCopia = { name: "Sales", size: 10, describe: departamento.describe };
+departamentoCopia.describe();
