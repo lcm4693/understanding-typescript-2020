0a7bd2fc55973fb35c0dd774aa315d98ef83d1a6
Diego Serpa <diegos.java@gmail.com>
2020-01-15 12:38:53 -0300

    Optional Parameters - Aula 77

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -1,38 +1,50 @@
-
 interface Named {
-    readonly name: string;
+  readonly name?: string;
+
+  // Parâmetrio opcional que não precisa ser implementado nas classes.
+  outputName?: string;
 }
 
 interface Greetable extends Named {
-    greet(phrase: string): void;
+  greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-    name: string;
-    
-    constructor(name: string, private age: number = 30){
-        this.name = name;
-    }
+  name?: string;
+  age: number = 30;
 
-    greet(phrase: string): void {
-        console.log(phrase + ' ' + this.name);
+  //Coloquei o parâmetro name como opcional através do símbolo '?'
+  constructor(name?: string) {
+    if (name) {
+      this.name = name;
     }
+  }
+
+  greet(phrase: string): void {
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
+  }
 }
 let user: Greetable;
 
-user = new Person('Diego');
+// Como o parâmetro name do construtor é opcional, posso chamar o construtor sem passar nada.
+user = new Person();
+// user = new Person('Diego');
 
 // Não posso alterar o atributo name, pois ele está definido na interface como readonly.
 // user.name = '';
 
-user.greet('Hi there! I\'m');
+user.greet("Hi there! I'm");
 
 // Estou criando uma interface para determinar funções.
 // Esse código comentado tem o mesmo funcionamento que o da interface.
 // type typeFunction = (a: number, b: number) => number;
 interface AddFn {
-    (a: number, b: number): number;
+  (a: number, b: number): number;
 }
 let addFn: AddFn = (a: number, b: number) => {
-    return a + b;
-}
+  return a + b;
+};
