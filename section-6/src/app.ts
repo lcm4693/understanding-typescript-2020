202a221645a58a1101fa2e129f0da4702d532366
Diego Serpa <diegos.java@gmail.com>
2020-01-17 16:09:48 -0300

    Discriminated Unions - Aula 84

--- section-6/src/app.ts
+++ section-6/src/app.ts
@@ -1,12 +1,11 @@
-
 type Admin = {
-    name: string;
-    privileges: string[];
+  name: string;
+  privileges: string[];
 };
 
 type Employee = {
-    name: string;
-    startDate: Date;
+  name: string;
+  startDate: Date;
 };
 
 //This new type is the combination between types Admin and Employee
@@ -15,10 +14,10 @@ type ElevatedEmployee = Admin & Employee;
 
 // Here, I created an object with all properties of all types.
 const e1: ElevatedEmployee = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date(),
-}
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
 
 type Combinable = string | number;
 type Numeric = number | boolean;
@@ -26,3 +25,94 @@ type Numeric = number | boolean;
 // This time I used intersectional type, so the Universal type is the intersection between these variables.
 // In this case, it will be a numeric variable, cause there is number type on these two variables.
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ', emp.name);
+
+  // I can't use the typeof, cause it's allowed to use only in known javascript types (number, string, boolean and object)
+  // So, I used the keyword 'in' to identify if the object has 'privileges' propertie.
+  if ('privileges' in emp) {
+    console.log('Privileges: ', emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start Date: ', emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive(){
+        console.log('Driving a car...');
+    }
+}
+
+class Truck {
+    drive(){
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number){
+        console.log('Loading cargo: ', amount);
+    }
+}
+
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle){
+    vehicle.drive();
+
+    // It's the same behavior to keyword 'in', but it's mor ellegant.
+    if(vehicle instanceof Truck){
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  flyingSpeed: number;
+  type: 'bird';
+}
+
+interface Horse {
+    runningSpeed: number;
+    type: 'horse';
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal (animal: Animal){
+  let speed: number;
+  switch(animal.type){
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+    default:
+      speed = 0;
+      break;
+  }
+  console.log('Moving at speed: ', speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 5});
+moveAnimal({type: 'horse', runningSpeed: 10});
