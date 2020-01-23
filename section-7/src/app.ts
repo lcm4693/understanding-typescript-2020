12c5413871e1b8d889ec44fb1ac7bc5730996d5a
Diego Serpa <diegos.java@gmail.com>
2020-01-23 20:35:30 -0300

    Working with constraints - Aula 95

--- section-7/src/app.ts
+++ section-7/src/app.ts
@@ -16,7 +16,8 @@ promise.then((data) => {
     // data.split(' ');
 });
 
-function merge<T, U>(obj1: T, obj2: U){
+// When I put the 'extends object' I'm telling for Typescript that it have to be an object.
+function merge<T extends object, U extends object>(obj1: T, obj2: U){
   // Method that merge two other objects.
   return Object.assign(obj1, obj2);
 }
