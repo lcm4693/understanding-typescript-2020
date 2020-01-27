81dc6ce1f1d35a965c55bb0a23bfd29197b2d9e6
Diego Serpa <diegos.java@gmail.com>
2020-01-27 19:07:07 -0300

    Another Generic Function - Aula 96

--- section-7/src/app.ts
+++ section-7/src/app.ts
@@ -10,21 +10,39 @@ const promise: Promise<number> = new Promise((resolve, reject) => {
   }, 2000);
 });
 
-promise.then((data) => {
-    console.log(data);
-    // The Typescript knows that data is a number, so I can't use a string method.
-    // data.split(' ');
+promise.then(data => {
+  console.log(data);
+  // The Typescript knows that data is a number, so I can't use a string method.
+  // data.split(' ');
 });
 
 // When I put the 'extends object' I'm telling for Typescript that it have to be an object.
-function merge<T extends object, U extends object>(obj1: T, obj2: U){
+function merge<T extends object, U extends object>(obj1: T, obj2: U) {
   // Method that merge two other objects.
   return Object.assign(obj1, obj2);
 }
 
-const united = merge({id: 1, hobbies: ['Sports']}, {name: 'Diego'});
+const united = merge({ id: 1, hobbies: ['Sports'] }, { name: 'Diego' });
 
-// I used generic function, so the return is union between T and U. 
+// I used generic function, so the return is union between T and U.
 // Now, Typescript can recognize the attributes like 'name' and 'id'.
 console.log(united.name);
 
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+
+  return [element, descriptionText];
+}
+
+// It works cause string type has a length propertie.
+console.log(countAndDescribe('Hi there'));
